5918ad0406f2603314bbfbf09daa6fd185f8c059
iashchuk <221box@list.ru>
2019-01-26 04:32:44 +0300

    Добавляет компонент Task

--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -1,8 +1,18 @@
 // Core
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
 // Instruments
-import Styles from './styles.m.css';
+import Styles from "./styles.md.css";
+
+// Components
+import Checkbox from "../../theme/assets/Checkbox";
+import Remove from "../../theme/assets/Remove";
+import Edit from "../../theme/assets/Edit";
+import Star from "../../theme/assets/Star";
+
+const PALETTE_COLOR_1 = "#000";
+const PALETTE_COLOR_2 = "#fff";
+const PALETTE_COLOR_3 = "#3b8ef3";
 
 export default class Task extends PureComponent {
     _getTaskShape = ({
@@ -18,6 +28,41 @@ export default class Task extends PureComponent {
     });
 
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        const { message } = this.props;
+
+        return (
+            <li className = { Styles.task }>
+                <div className = { Styles.content }>
+                    <Checkbox
+                        inlineBlock
+                        className = { Styles.toggleTaskCompletedState }
+                        color1 = { PALETTE_COLOR_3 }
+                        color2 = { PALETTE_COLOR_2 }
+                    />
+                    <input disabled type = 'text' value = { message } />
+                </div>
+                <div className = { Styles.actions }>
+                    <Star
+                        inlineBlock
+                        checked = { false }
+                        className = { Styles.toggleTaskFavoriteState }
+                        color1 = { PALETTE_COLOR_3 }
+                        color2 = { PALETTE_COLOR_1 }
+                    />
+                    <Edit
+                        inlineBlock
+                        checked = { false }
+                        className = { Styles.updateTaskMessageOnClick }
+                        color1 = { PALETTE_COLOR_3 }
+                        color2 = { PALETTE_COLOR_1 }
+                    />
+                    <Remove
+                        inlineBlock
+                        color1 = { PALETTE_COLOR_3 }
+                        color2 = { PALETTE_COLOR_1 }
+                    />
+                </div>
+            </li>
+        );
     }
 }
