c267a6586704989f1de01270de90f4044d9a6e81
iashchuk <221box@list.ru>
2019-02-11 03:17:11 +0300

    Реализует добавление задачи

--- source/components/Form/index.js
+++ source/components/Form/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    state = {
+        message: '',
+    };
+
+    onChange = (evt) => {
+        this.setState({ [evt.target.name]: evt.target.value });
+    };
+
+    onSubmit = (evt) => {
+        const { message } = this.state;
+        const { onAdd } = this.props;
+
+        evt.preventDefault();
+        onAdd({
+            id:        Date.now(),
+            message,
+            completed: false,
+            favorite:  false,
+        });
+        this.setState({ message: '' });
+    };
+
     render () {
+        const { message } = this.state;
+
         return (
-            <form>
+            <form onSubmit = { this.onSubmit }>
                 <input
+                    required
                     maxLength = { 50 }
+                    minLength = { 4 }
+                    name = 'message'
                     placeholder = 'Описание моей новой задачи'
                     type = 'text'
+                    value = { message }
+                    onChange = { this.onChange }
                 />
                 <button>Добавить задачу</button>
             </form>
--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,22 +1,28 @@
 // Core
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 // Instruments
-import Styles from "./styles.md.css";
-import { api, data } from "../../REST"; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
+import Styles from './styles.md.css';
+import { api, data } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
 // Components
-import Form from "../Form";
-import Title from "../Title";
-import Search from "../Search";
-import CompleteIndicator from "../CompleteIndicator";
-import TodoList from "../TodoList";
+import Form from '../Form';
+import Title from '../Title';
+import Search from '../Search';
+import CompleteIndicator from '../CompleteIndicator';
+import TodoList from '../TodoList';
 
-const APP_NAME = "Планировщик задач";
+const APP_NAME = 'Планировщик задач';
 
 export default class Scheduler extends Component {
     state = {
         tasks: data,
+    };
+
+    onAdd = (task) => {
+        const { tasks } = this.state;
+
+        this.setState({ tasks: [task, ...tasks]});
     };
 
     render () {
@@ -30,7 +36,7 @@ export default class Scheduler extends Component {
                         <Search />
                     </header>
                     <section>
-                        <Form />
+                        <Form onAdd = { this.onAdd } />
                         <TodoList tasks = { tasks } />
                     </section>
                     <footer>
